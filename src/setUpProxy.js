3e5bf919b92fac6e4d5c3e8eb5da4a423c39c022
Alexey Rybkin <111182781+GattlerGun@users.noreply.github.com>
2023-09-09 12:11:07 +1000

    Update setUpProxy.js

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 module.exports = app => {
     app.use(
         createProxyMiddleware('v4/games', {
-            target: 'https://api.igdb.com',
+            target: 'https://api.igdb.com/v4/games',
             changeOrigin: true,
         })
     )
